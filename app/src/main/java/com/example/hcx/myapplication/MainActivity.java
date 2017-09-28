73933c67626926f0f7e303c21b9beeae2ca7fc22
hcx <hcx666@pku.edu.cn>
2017-09-28 20:42:18 +0800

    测试程序

--- app/src/main/java/com/example/hcx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hcx/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.hcx.myapplication;
 
 import android.app.Activity;
-
+import android.os.Bundle;
 /**
  * Created by hcx on 2017/9/28.
  */
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+    }
 }
